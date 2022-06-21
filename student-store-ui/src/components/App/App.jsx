874d45e9a600509cb2ed6830fb0cdfde101b34ef
Daniel Ortiz <daniel.ortiz@danielo-ltmu23e.internal.salesforce.com>
2022-06-21 15:12:06 -0700

    Routers Complete

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -2,19 +2,40 @@ import * as React from "react"
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
+import ProductDetail from "../ProductDetail/ProductDetail"
+import NotFound from "../NotFound/NotFound"
 import "./App.css"
+import { useState } from "react"
+
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
+
+  const [products, setProducts] = useState([0])
+  const [isFetching, setIsFetching] = useState(0)
+  const [error, setError] = useState("")
+  const [isOpen, setIsOpen] = useState(0)
+  const [shoppingCart, setShoppingCart] = useState(null)
+  const [checkoutForm, setCheckoutForm] = useState(null)
+
+
   return (
     <div className="app">
-      <BrowserRouter>
         <main>
-          {/* YOUR CODE HERE! */}
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products/:productId" element={<ProductDetail />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
           <Navbar />
           <Sidebar />
           <Home />
         </main>
-      </BrowserRouter>
+      
     </div>
   )
 }
---
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import "./ProductDetail.css"
+
+export default function ProductDetail() {
+  return (
+    <div className="ProductDetail">
+      <p>Products available today</p>
+    </div>
+  )
+}
